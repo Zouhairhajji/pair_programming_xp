be83a034d5e07a22cbf7139f075a4db09398e6f6
Zouhair HAJJI <zouhair.hajji@outlook.fr>
2019-02-14 16:47:42 +0100

    add changes

---
+++ src/test/java/fr/dauphine/zoo/RunMapTest.java
@@ -0,0 +1,12 @@
+package fr.dauphine.zoo;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, monochrome = true)
+public class RunMapTest {
+    
+}
